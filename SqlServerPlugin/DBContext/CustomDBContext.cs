d4edb4247cacc5a6b14b6b1fc6bff5f56e07bd68
Dohy1080 <huydan473@gmail.com>
2025-03-06 13:00:20 +0700

    Add files via upload

---
+++ SqlServerPlugin/DBContext/CustomDBContext.cs
@@ -0,0 +1,21 @@
+﻿using CoreEntities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlServerPlugin
+{
+    public class CustomDBContext : DbContext
+    {
+        public CustomDBContext(DbContextOptions<CustomDBContext> options) : base(options) { }
+
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Score> Scores { get; set; }
+
+    }
+}
---
+++ SqlServerPlugin/TeacherRepository.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLogic.Repository;
+using CoreEntities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlServerPlugin
+{
+    public class TeacherRepository : ITeacherRepository
+    {
+        private CustomDBContext _dbContext;
+        public TeacherRepository(CustomDBContext dBContext)
+        {
+            _dbContext = dBContext;
+        }
+        public List<Teacher> GetAllTeachers()
+        {
+            return _dbContext.Teachers.ToList();
+        }
+
+    }
+}
